e2f11c1a298a0b793852eb0b43a46bd5b24f00fc
hzf <huangzhenfang2017@163.com>
2020-02-11 17:59:36 +0800

    测试数据 创建超级用户 创建后台管理

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 # 有了模型类之后模型类如何与数据库交互
 # 1.注册模型 在setting.py 中的INSTALLED_APPS 添加应用名 booktest
@@ -14,23 +15,27 @@ class Book(models.Model):
     pub_date = models.DateField(default="1983-06-01")
     price = models.FloatField(default=0)
     pass
+
     def __str__(self):
         return self.title
+
 
 class Hero(models.Model):
     """
     Hero 继承了Model 也可以操作数据库
     """
     name = models.CharField(max_length=20)
-    gender = models.CharField(max_length=6, choices=(("male","男"),("female","女")),default="male")
+    gender = models.CharField(max_length=6, choices=(("male", "男"), ("female", "女")), default="male")
     content = models.CharField(max_length=100)
     # book是一对多中的外键 on_delete代表删除主表数据时如何做，models.CASCADE 代表级联效应  删除主键连同外键一起删除
-    book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
     pass
+
     def __str__(self):
         return self.name
+
+
 # django orm 关联查询
 # 多方Hero  一方Book
 # 1.多找一， 多方对象，关系字段       exp：h1.book
 # 2. 一找多，  一方对象，小写多方类名_set.all()   exp : b1.hero_set.all()
-
