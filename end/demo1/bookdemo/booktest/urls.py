bd873c5e0b5b2559a32d9f70d4bdc573df668be0
hzf <huangzhenfang2017@163.com>
2020-02-14 20:23:35 +0800

    添加英雄 添加书籍

--- end/demo1/bookdemo/booktest/urls.py
+++ end/demo1/bookdemo/booktest/urls.py
@@ -16,6 +16,12 @@ urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^about/$',views.about,name='about'),
     # 使用正则分组传递参数
-    url(r'^detail/(\d+)',views.detail,name='detail')
+    url(r'^detail/(\d+)$',views.detail,name='detail'),
+
+    url(r'^deletebook/(\d+)$',views.deletebook,name='deletebook'),
+    url(r'^deletehero/(\d+)$',views.deletehero,name='deletehero'),
+    url(r'^addhero/(\d+)$',views.addhero,name='addhero'),
+    url(r'^edithero/(\d+)$',views.edithero,name='edithero'),
+    url(r'^addbook/$',views.addbook,name='addbook'),
 
 ]
--- end/demo1/bookdemo/booktest/views.py
+++ end/demo1/bookdemo/booktest/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,reverse
 from django.template import loader
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Book,Hero
-def index(response):
+def index(request):
     # return HttpResponse("这里是首页")
     # 1.获取模板
     # template = loader.get_template('index.html')
@@ -15,8 +15,8 @@ def index(response):
     # return HttpResponse(result)
 
     # 3步合成一部
-    return render(response,'index.html',{'books':books})
-def detail(response,bookid):
+    return render(request,'index.html',{'books':books})
+def detail(request,bookid):
     # return HttpResponse("这里是详情页"+ bookid)
     # template = loader.get_template('detail.html')
     book = Book.objects.get(id = bookid)
@@ -24,6 +24,59 @@ def detail(response,bookid):
     # result = template.render(context)
     # return HttpResponse(result)
 
-    return render(response,'detail.html',{'book':book})
+    return render(request,'detail.html',{'book':book})
 def about(response):
     return HttpResponse("这里是关于")
+
+def deletebook(request,bookid):
+    book = Book.objects.get(id = bookid)
+    book.delete()
+    # return HttpResponseRedirect(redirect_to='/')
+    url = reverse("booktest:index")
+    return redirect(to=url)
+
+def deletehero(request,heroid):
+    hero = Hero.objects.get(id = heroid)
+    bookid = hero.book.id
+    hero.delete()
+    url = reverse("booktest:detail",args=(bookid,))
+    return redirect(to=url)
+
+def addhero(request,bookid):
+    if request.method == 'GET':
+        return render(request,'addhero.html')
+    elif request.method == 'POST':
+        hero = Hero()
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("herosex")
+        hero.book = Book.objects.get(id = bookid)
+        hero.save()
+        url = reverse("booktest:detail",args=(bookid,))
+        return redirect(to=url)
+
+def edithero(request,heroid):
+    hero = Hero.objects.get(id = heroid)
+    if request.method == "GET":
+        return render(request,'edithero.html',{'hero':hero})
+    elif request.method == "POST":
+        hero.name = request.POST.get("heroname")
+        hero.content = request.POST.get("herocontent")
+        hero.gender = request.POST.get("herosex")
+        hero.save()
+        url = reverse("booktest:detail",args=(hero.book.id,))
+        return redirect(to=url)
+
+def addbook(request):
+    if request.method == "GET":
+        return render(request,'addbook.html')
+    elif request.method == "POST":
+        book = Book()
+        book.title = request.POST.get("booktitle")
+        book.pub_date = request.POST.get("bookpub_date")
+        book.price = request.POST.get("bookprice")
+        book.author = request.POST.get("bookauthor")
+        book.save()
+        url = reverse("booktest:index")
+        return redirect(to=url)
+    pass
