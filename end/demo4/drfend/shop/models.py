92266e295070126c01938175aa84d47006bceaf9
hzf <huangzhenfang2017@163.com>
2020-02-26 17:39:04 +0800

    djangoRestFrameWork框架练习

---
+++ end/demo4/drfend/shop/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, verbose_name='分类名')
+
+    def __str__(self):
+        return self.name
+
+
+class Good(models.Model):
+    name = models.CharField(max_length=20, verbose_name='商品名')
+    desc = models.CharField(max_length=100, null=True, blank=True, verbose_name='商品描述')
+    #  在序列化关联模型一定要声明 relate_name
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='分类',related_name='goods')
+
+    def __str__(self):
+        return self.name
---
+++ end/demo4/drfend/shop/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+# Create your views here.
+
+class CategoryViewSets(viewsets.ModelViewSet):
+    """
+    分类视图
+    继承ModelViewSet 之后拥有GET POST PUT PATCH DELETE 等HTTP动作
+    queryset 指明 需要操作的模型表
+
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class GoodViewSets(viewsets.ModelViewSet):
+    queryset = Good.objects.all()
+    serializer_class = GoodSerializer
