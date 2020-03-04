5eb28535f00f550a5bdd07d3cef5b84559fafcd7
hzf <huangzhenfang2017@163.com>
2020-03-04 19:46:55 +0800

    博客学习

---
+++ end/demo2/blog/apps/blogapp/search_indexes.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+"""
+@File    : search_indexes.py
+@Time    : 2020/2/24 9:19
+@Author  : hzf
+@Email   : huangzhenfang2017@163.com
+@Software: PyCharm
+"""
+from haystack import indexes
+# 指明搜索的模型为Article
+from .models import Article
+# 1。类名必须为模型名Index
+# 2.get_model 必须返回模型
+class ArticleIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    def get_model(self):
+        return Article
+    def index_queryset(self, using=None):
+        return self.get_model().objects.all()
